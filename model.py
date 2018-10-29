284d325c50f62f9bb4fd1fa0f455366a81abdc3b
takam5f2 <taka.akamine@gmail.com>
2018-10-29 19:46:47 +0900

    update each editable files.

--- model.py
+++ model.py
@@ -23,10 +23,35 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+        super(DecoderRNN, self).__init__()
+        drop_prob = 0.5
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers,
+                           dropout=drop_prob, batch_first=True)
+        self.dropout = nn.Dropout(drop_prob)
+        self.fc = nn.Linear(hidden_size, vocab_size)
+        self.init_weights()
+
+    def init_weights(self):
+        ''' Initialize weights for fully connected layer '''
+        # Embedding Weights as random uniform
+        self.embedding.weight.data.uniform_(-0.1, 0.1)
+
+        # Set bias tensor to all zeros
+        self.fc.bias.data.fill_(0)
+        # FC weights as random uniform
+        self.fc.weight.data.uniform_(-0.1, 0.1)
+
     def forward(self, features, captions):
-        pass
+        captions = captions[:,:captions.shape[1]-1]
+        caption_embed = self.embedding(captions)
+        x = torch.cat((torch.unsqueeze(features, 1), caption_embed), 1)
+        out, _ = self.lstm(x)
+        out = self.dropout(out)
+        # out = out.view(-1, self.hidden_size)
+        out = self.fc(out)
+        return out
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
